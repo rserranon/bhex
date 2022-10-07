f516ee361b132240c155334cc03b18db49d7e622
Robert Serrano <rserranon@gmail.com>
2022-10-07 16:26:02 -0500

    Fix bug with setfill

--- bhex.cpp
+++ bhex.cpp
@@ -3,7 +3,7 @@
 #include <stack>
 #include <iostream>
 #include <sstream>
-
+#include <iomanip>
 
 
 
@@ -19,27 +19,26 @@ int main( int argc, char* argv[] )
             return -1;
         }
 
-  std::string   hex = argv[1];
-  if (hex.length() % 2 != 0 ) 
+  std::string   hex_str = argv[1];
+  if (hex_str.length() % 2 != 0 ) 
     {
         std::cout << "It only works with even srings. Usage: bhex 'hexstring'" << std::endl;
         return -1;
     }
-  for (unsigned int i = 0; i < hex.length(); i += 2) {
-    std::string byteString = hex.substr(i, 2);
+  for (unsigned int i = 0; i < hex_str.length(); i += 2) {
+    std::string byteString = hex_str.substr(i, 2);
     unsigned char byte = (unsigned char) strtol(byteString.c_str(), NULL, 16);
     bytes.push(byte);
   }
 
-  std::cout << "original:"<< hex << std::endl;
+  std::cout << "original:"<< hex_str << std::endl;
   std::cout << "bhex    :";
-
+  std::cout << std::setfill('0') << std::setw(2);
   while ( !bytes.empty() )
   {
-      std::cout << std::hex << int(bytes.top());
+      printf("%02x", static_cast<int>(bytes.top()));
       bytes.pop();
   }
   std::cout << std::endl;
-
   return 0;
 }

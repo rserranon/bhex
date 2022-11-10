90eacc85a0b8623ea6bb2591b54e694edfb6b9f1
Robert Serrano <rserranon@gmail.com>
2022-11-09 22:23:07 -0600

    Formated with ClangFormat
    Formated source code using ClangFormat plugin in Vim

--- bhex.cpp
+++ bhex.cpp
@@ -9,36 +9,35 @@
 
 int main( int argc, char* argv[] )
 {
-  std::stack<unsigned char> bytes;
-  if (argc > 2 ) {
+    std::stack<unsigned char> bytes;
+    if (argc > 2) {
         std::cout << "Too many arguments. Usage: bhex 'hexstring'" << std::endl;
         return -1;
-  } else 
-        if (argc < 2) {
-            std::cout << "Too few arguments. Usage: bhex 'hexstring'" << std::endl;
-            return -1;
-        }
-
-  std::string   hex_str = argv[1];
-  if (hex_str.length() % 2 != 0 ) 
-    {
-        std::cout << "It only works with even srings. Usage: bhex 'hexstring'" << std::endl;
+    } else if (argc < 2) {
+        std::cout << "Too few arguments. Usage: bhex 'hexstring'" << std::endl;
         return -1;
     }
-  for (unsigned int i = 0; i < hex_str.length(); i += 2) {
-    std::string byteString = hex_str.substr(i, 2);
-    unsigned char byte = (unsigned char) strtol(byteString.c_str(), NULL, 16);
-    bytes.push(byte);
-  }
 
-  std::cout << "original:"<< hex_str << std::endl;
-  std::cout << "bhex    :";
-  std::cout << std::setfill('0') << std::setw(2);
-  while ( !bytes.empty() )
-  {
-      printf("%02x", static_cast<int>(bytes.top()));
-      bytes.pop();
-  }
-  std::cout << std::endl;
-  return 0;
+    std::string hex_str = argv[1];
+    if (hex_str.length() % 2 != 0) {
+        std::cout << "It only works with even srings. Usage: bhex 'hexstring'"
+                  << std::endl;
+        return -1;
+    }
+    for (unsigned int i = 0; i < hex_str.length(); i += 2) {
+        std::string byteString = hex_str.substr(i, 2);
+        unsigned char byte =
+            (unsigned char)strtol(byteString.c_str(), NULL, 16);
+        bytes.push(byte);
+    }
+
+    std::cout << "original:" << hex_str << std::endl;
+    std::cout << "bhex    :";
+    std::cout << std::setfill('0') << std::setw(2);
+    while (!bytes.empty()) {
+        printf("%02x", static_cast<int>(bytes.top()));
+        bytes.pop();
+    }
+    std::cout << std::endl;
+    return 0;
 }
